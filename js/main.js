a5933abd92134930b55c62d403126ec944069203
kanry11x1997 <kanry11x1997@gmail.com>
2025-10-26 22:39:34 +0700

    xoa js deploy

--- js/main.js
+++
@@ -1,7 +0,0 @@
-// js/main.js
-import "./app/guest/guest.js";
-import "./app/guest/comfirmInfor.js";
-import "./app/guest/gallery-more.js";
-import "./app/guest/audio.js";
-import "./app/guest/image.js";
-import "./app/guest/video.js";
--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-// postcss.config.js
-const purgecss = require('@fullhuman/postcss-purgecss');
-
-module.exports = {
-  plugins: [
-    require("autoprefixer"),
-    require("cssnano")({ preset: "default" })
-  ]
-};
